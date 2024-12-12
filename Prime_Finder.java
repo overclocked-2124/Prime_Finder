68f4254a189be939d2e9459153bff45772e2c968
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2024-12-12 18:48:46 +0530

    Add files via upload
    Added the main java code file

---
+++ Prime_Finder.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Prime_Finder{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] Prime = new int[n];
+        Prime[0]=2;
+        int i,j,count;
+        count=1;
+
+        System.out.println("Hello");
+        System.out.println(Prime[3]);
+        for(i=3;i<n;i++){
+            boolean is_prime = true;
+            for(j=0;j<n;j++){
+                if(Prime[j]!=0){
+                    if(i%Prime[j]==0){
+                        is_prime=false;
+                    }
+                }
+                else{break;}
+            }
+            if(is_prime){
+                Prime[count]=i;
+                count++;
+            }
+        }
+        
+        for(i=0;i<count;i++){
+            System.out.print(Prime[i]+"  ");
+        }
+
+
+    }
+}
